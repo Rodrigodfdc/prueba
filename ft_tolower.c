727e6199b159787632be4c1ea3f26c2a8a74f3f7
Rodrigo De Freitas Da Cruz <rodde-fr@c1r4s5.42madrid.com>
2026-02-06 21:11:33 +0100

    ft_tolower

---
+++ ft_tolower.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rodde-fr <rodde-fr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/01 19:00:15 by rodde-fr          #+#    #+#             */
+/*   Updated: 2026/02/06 21:10:13 by rodde-fr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_tolower(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (c + 32);
+	return (c);
+}
+
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	int	i;
+
+	i = 'A';
+	printf("Antes del ft_tolower %c\n", i);
+	i = ft_tolower(i);
+	printf("Después del ft_tolower %c \n", i);
+	return (0);
+}
+*/
