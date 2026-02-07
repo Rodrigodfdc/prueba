1d0423c0077ed21373bb8e57ce4875832990d085
Rodrigodfdc <120066361+Rodrigodfdc@users.noreply.github.com>
2026-02-07 12:41:11 +0000

    ft_atoi

---
+++ ft_atoi.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rodde-fr <rodde-fr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/01 18:47:59 by rodde-fr          #+#    #+#             */
+/*   Updated: 2026/02/01 18:51:25 by rodde-fr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '\r'
+		|| str[i] == '\v' || str[i] == '\f')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
+
+
+#include <stdio.h>
+
+int	main(void)
+{
+	char	str[] = "   -42";
+	int		result;
+
+	result = ft_atoi(str);
+	printf("Result: %d\n", result);
+	return (0);
+}
