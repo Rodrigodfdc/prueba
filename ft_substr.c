e7a2f9adc448a74fa5e0bc8a66674ebfc6edca6e
Rodrigo De Freitas Da Cruz <rodde-fr@c1r6s5.42madrid.com>
2026-02-07 21:39:08 +0100

    ft_substr.c

---
+++ ft_substr.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rodde-fr <rodde-fr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/01 19:00:08 by rodde-fr          #+#    #+#             */
+/*   Updated: 2026/02/07 21:38:32 by rodde-fr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	str_len;
+
+	if (!s)
+		return (NULL);
+	str_len = ft_strlen((char *)s);
+	if (start >= str_len)
+		return (ft_strdup(""));
+	if (len > str_len - start)
+		len = str_len - start;
+	substr = (char *)malloc(len + 1);
+	if (!substr)
+		return (NULL);
+	ft_strlcpy(substr, s + start, len + 1);
+	return (substr);
+}
+
+/*
+int	main(void)
+{
+	char	*s;
+	char	*substr;
+
+	s = "Hola Mundo";
+	substr = ft_substr(s, 5, 4);
+	if (substr)
+	{
+		printf("Substring: %s\n", substr);
+		free(substr);
+	}
+	else
+	{
+		printf("Error reservando memoria para el substring\n");
+	}
+	return (0);
+}
+*/
