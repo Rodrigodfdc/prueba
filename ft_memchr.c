4689fa83681369671b2883ee8cc9bd31987e057a
Rodrigodfdc <120066361+Rodrigodfdc@users.noreply.github.com>
2026-02-07 11:19:54 +0000

    ft_memchr

---
+++ ft_memchr.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rodde-fr <rodde-fr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/01 18:54:21 by rodde-fr          #+#    #+#             */
+/*   Updated: 2026/02/01 18:54:24 by rodde-fr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*str;
+
+	str = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		if (str[i] == (unsigned char)c)
+			return ((void *)(str + i));
+		i++;
+	}
+	return (0);
+}
+
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char	str[] = "Hola Mundo";
+	char	*result;
+
+	result = ft_memchr(str, 'M', 13);
+	if (result != 0)
+		printf("Encontrado: %s\n", result);
+	else
+		printf("No Encontrado\n");
+	return (0);
+}
+*/
