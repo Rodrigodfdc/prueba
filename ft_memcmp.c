0dc5b098e0506290f3e42dbcc7695efa98729160
Rodrigodfdc <120066361+Rodrigodfdc@users.noreply.github.com>
2026-02-07 11:43:47 +0000

    ft_memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rodde-fr <rodde-fr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/01 18:54:32 by rodde-fr          #+#    #+#             */
+/*   Updated: 2026/02/01 18:54:33 by rodde-fr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*str1;
+	const unsigned char	*str2;
+
+	str1 = (const unsigned char *)s1;
+	str2 = (const unsigned char *)s2;
+	while (n--)
+	{
+		if (*str1 != *str2)
+			return (*str1 - *str2);
+		str1++;
+		str2++;
+	}
+	return (0);
+}
+
+
+#include <stdio.h>
+
+int	main(void)
+{
+	char	str1[] = "Hali";
+	char	str2[] = "Hola";
+	int		n;
+	int		result;
+
+	n = 5;
+	result = ft_memcmp(str1, str2, n);
+	printf("Result: %d\n", result);
+	return (0);
+}
