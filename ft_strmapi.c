4bf38ca43a126b65bfd975a8c1663dacba25b734
Rodrigodfdc <120066361+Rodrigodfdc@users.noreply.github.com>
2026-02-09 20:00:40 +0000

    ft_strmapi.c

---
+++ ft_strmapi.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rodde-fr <rodde-fr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/01 18:59:20 by rodde-fr          #+#    #+#             */
+/*   Updated: 2026/02/01 18:59:22 by rodde-fr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*result;
+	unsigned int	i;
+
+	if (!s || !f)
+		return (NULL);
+	result = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		result[i] = f(i, s[i]);
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
+
+/*
+int	main(void)
+{
+	char	str[] = "jotaro";
+	char	*result;
+    // no le gusta que el que le pase una función que solo use un int,
+    // pero no voy a hacer una función aparte
+	result = ft_strmapi(str, ft_toupper);
+	printf("%s\n", result);
+	free(result);
+	return (0);
+}
+*/
