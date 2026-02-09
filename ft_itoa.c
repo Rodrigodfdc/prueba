f97fbd4528e6c9b509a5be402d74010905893b76
Rodrigodfdc <120066361+Rodrigodfdc@users.noreply.github.com>
2026-02-09 17:03:12 +0000

    ft_atoi

---
+++ ft_itoa.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rodde-fr <rodde-fr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/01 18:52:40 by rodde-fr          #+#    #+#             */
+/*   Updated: 2026/02/01 18:52:42 by rodde-fr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_numlen(int n)
+{
+	int	len;
+
+	len = 0;
+	if (n <= 0)
+		len = 1;
+	while (n)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	int		len;
+	long	nb;
+
+	nb = n;
+	len = ft_numlen(n);
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	str[len--] = '\0';
+	if (nb < 0)
+	{
+		str[0] = '-';
+		nb = -nb;
+	}
+	while (len >= 0 && str[len] != '-')
+	{
+		str[len--] = (nb % 10) + '0';
+		nb /= 10;
+	}
+	return (str);
+}
+
+/*
+int	main(void)
+{
+	int		num;
+	char	*result;
+
+	num = -12345;
+	result = ft_itoa(num);
+	printf("El número %d como cadena es: %s\n", num, result);
+	free(result);
+	return (0);
+}
+*/
