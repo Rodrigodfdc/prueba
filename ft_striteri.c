295bfa726b3c9b2cca6077081d34ba9d97de76cf
Rodrigodfdc <120066361+Rodrigodfdc@users.noreply.github.com>
2026-02-09 21:15:46 +0000

    ft_striteri.c

---
+++ ft_striteri.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rodde-fr <rodde-fr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/01 18:57:51 by rodde-fr          #+#    #+#             */
+/*   Updated: 2026/02/01 18:57:52 by rodde-fr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
+{
+	unsigned int	i;
+
+	if (!s || !f)
+		return ;
+	i = 0;
+	while (s[i])
+	{
+		f(i, &s[i]);
+		i++;
+	}
+}
+
+/*
+int	main(void)
+{
+	char	str[] = "bug bunny";
+
+	ft_striteri(str, ft_toupper);
+	printf("%s\n", str);
+	return (0);
+}
+*/
