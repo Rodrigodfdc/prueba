bdf28f5b2a7cbd748cf9f3e1ed7a02f878653f70
Rodrigodfdc <120066361+Rodrigodfdc@users.noreply.github.com>
2026-02-09 23:16:24 +0000

    ft_putchar.c

---
+++ ft_putchar_fd.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rodde-fr <rodde-fr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/01 18:55:42 by rodde-fr          #+#    #+#             */
+/*   Updated: 2026/02/01 18:55:43 by rodde-fr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putchar_fd(char c, int fd)
+{
+	if (fd < 0)
+		return ;
+	write(fd, &c, 1);
+}
+
+/*
+int	main(void)
+{
+	char	c;
+
+	int fd = 1; // Salida estándar
+	c = 'A';
+	ft_putchar_fd(c, fd);
+	return (0);
+}
+*/
